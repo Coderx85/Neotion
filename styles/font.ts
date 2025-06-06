52ae394c01f8fe2ac02bfa24d0163165d4d266bd
Coderx85 <work.priyanshu085@gmail.com>
2025-06-06 23:59:38 +0530

    Refactor font definitions and add global styles; create env configuration file.

--- styles/font.ts
+++ styles/font.ts
@@ -2,12 +2,24 @@ import { Inter, Lora, Source_Sans_3 } from 'next/font/google'
 import localFont from 'next/font/local'
  
 // define your variable fonts
-const inter = Inter()
-const lora = Lora()
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["200"]
+})
+
+const lora = Lora({
+  subsets: ["latin"],
+  weight: ["400"]
+})
+
 // define 2 weights of a non-variable font
-const sourceCodePro400 = Source_Sans_3({ weight: '400' })
-const sourceCodePro700 = Source_Sans_3({ weight: '700' })
-// define a custom local font where GreatVibes-Regular.ttf is stored in the styles folder
-const greatVibes = localFont({ src: './GreatVibes-Regular.ttf' })
- 
-export { inter, lora, sourceCodePro400, sourceCodePro700, greatVibes }
+const sourceCodePro400 = Source_Sans_3({ 
+  subsets: ["latin"],
+  weight: '400' })
+
+const sourceCodePro700 = Source_Sans_3({ 
+  subsets: ["latin"],
+  weight: '700' }
+)
+
+export { inter, lora, sourceCodePro400, sourceCodePro700 }
---
+++ utils/config/env.ts
@@ -0,0 +1,11 @@
+export const config = {
+  env: {
+    // Authentication
+    CLERK_API_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY!,
+
+    // Database
+    CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL!,
+    CONVEX_DEPLOYMENT: process.env.CONVEX_DEPLOYMENT!,
+  }
+}
