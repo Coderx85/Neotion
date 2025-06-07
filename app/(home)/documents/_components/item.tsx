85c9a199cec57d35e737c6a9fe719f33b2caa9ba
Coderx85 <work.priyanshu085@gmail.com>
2025-06-07 22:11:42 +0530

    Add DocumentList and Item components;

--- app/(home)/documents/_components/item.tsx
+++ app/(home)/documents/_components/item.tsx
@@ -1,15 +1,43 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { Skeleton } from '@/components/ui/skeleton';
+import { Id } from '@/convex/_generated/dataModel';
+import { MoreHorizontal } from 'lucide-react';
 import React from 'react';
+import { FaPlus, FaTrash } from 'react-icons/fa6';
 import { IconType } from 'react-icons/lib';
 
 type ItemProps = {
-  label: string;
-  onClick: () => void;
-  icon: IconType;
+  id?: Id<'documents'>;
+  documentIcon?: string;
+  active?: boolean;
+  expanded?: boolean;
   isSearch?: boolean;
+  level?: number;
+  onExpand?: () => void;
+  label: string;
+  onClick?: () => void;
+  icon: IconType;
 };
 
-export default function Item ({ label, onClick, icon: Icon, isSearch }: ItemProps) {
+export default function Item({
+  label,
+  onClick,
+  icon: Icon,
+  isSearch,
+  id,
+  documentIcon,
+  active,
+  level,
+  onExpand,
+}: ItemProps) {
   return (
     <div
       role="button"
@@ -19,25 +47,64 @@ export default function Item ({ label, onClick, icon: Icon, isSearch }: ItemProp
       <Icon className="shrink-0 h-[14px] mr-2" />
       <span className="truncate">{label}</span>
       {isSearch && (
-        <kbd className='ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100'>
+        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-xs text-gray-500">⌘</span>K
         </kbd>
       )}
-    </div>
-  );
-};
-
-Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
-  return (
-    <div 
-      style={{
-        paddingLeft: level ? `${(level * 12)}px` : "12px"
-      }}
-      className='flex gap-x-2 py-[3px]'
-    >
-      <Skeleton className='size-4'/>
-
-      <Skeleton className='h-4 w-[30%]'/>
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild onClick={(e) => e.stopPropagation()}>
+              <div
+                className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm
+              hover:bg-neutral-300 dark:hover:bg-neutral-600"
+                role="button"
+              >
+                <MoreHorizontal className="w-4 h-4 text-muted-foreground" />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="w-60"
+              align="start"
+              side="right"
+              forceMount
+            >
+              <DropdownMenuItem 
+                // onClick={onArchive}
+              >
+                <FaTrash className="w-4 h-4 mr-2" />
+                Delete
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <div className="text-xs text-muted-foreground p-2">
+                Last edited by: {'User Name'}
+              </div>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <div
+            className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+            role="button"
+            // onClick={onCreate}
+          >
+            <FaPlus className="w-4 h-4 text-muted-foreground" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
+
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+  return (
+    <div
+      style={{
+        paddingLeft: level ? `${level * 12}px` : '12px',
+      }}
+      className="flex gap-x-2 py-[3px]"
+    >
+      <Skeleton className="size-4" />
+
+      <Skeleton className="h-4 w-[30%]" />
+    </div>
+  );
+};
