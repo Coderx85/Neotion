fa2558156dd7083b285e79fbe0c0e226ce8239bb
Coderx85 <work.priyanshu085@gmail.com>
2025-06-13 08:50:16 +0530

    Updated existing dependencies to their latest versions.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,18 +6,21 @@ interface NavbarProps {
 }
 import { useParams } from 'next/navigation';
 import { useQuery } from 'convex/react';
+import { useMediaQuery } from 'usehooks-ts';
 import { MenuIcon } from 'lucide-react';
 
 import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel';
+import { cn } from '@/utils/lib';
 
-// import { Title } from './Title';
+import { Title } from './Title';
 // import { Banner } from './Banner';
 // import { Menu } from './Menu';
 // import { Publish } from './Publish';
 
 export function Navbar({ isCollapsed, onResetWidthAction }: NavbarProps) {
   const params = useParams();
+  const isMobile = useMediaQuery('(max-width:768px)');
 
   const document = useQuery(api.document.getById, {
     documentId: params.documentId as Id<'documents'>,
@@ -26,11 +29,12 @@ export function Navbar({ isCollapsed, onResetWidthAction }: NavbarProps) {
   if (document === undefined) {
     return (
       <nav
-        className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full
-      flex justify-between gap-x-4"
+        className={cn(
+          'bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex justify-between gap-x-4',
+          isMobile && 'p-0'
+        )}
       >
-        {/* <Title.Skeleton /> */}
-        Title Section
+        <Title.Skeleton />
         <div className="flex gap-x-2 items-center">
           {/* <Menu.Skeleton /> */}
           Menu Section
@@ -46,18 +50,20 @@ export function Navbar({ isCollapsed, onResetWidthAction }: NavbarProps) {
   return (
     <>
       <nav
-        className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full
-      flex gap-x-4 items-center"
+        className={cn(
+          'bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex justify-between gap-x-4',
+          isMobile && 'p-0'
+        )}
       >
         {isCollapsed && (
           <MenuIcon
-            className="w-6 h-6 text-muted-foreground"
+            className="w-6 h-6 text-muted-foreground cursor-pointer"
             role="button"
             onClick={onResetWidthAction}
           />
         )}
         <div className="flex justify-between items-center w-full">
-          {/* <Title initialData={document} /> */}
+          <Title initialData={document} />
           <div className="flex gap-x-2 items-center">
             {/* <Publish initialData={document} /> */}
             {/* <Menu documentId={document._id} /> */}
