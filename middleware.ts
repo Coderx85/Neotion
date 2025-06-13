183703df69fab2fefeb476d0415bedbd970e72bc
Coderx85 <work.priyanshu085@gmail.com>
2025-06-13 13:57:14 +0530

    feat: Update button styles for dark mode support.

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher(['/', '/forum(.*)']);
+const isProtectedRoute = createRouteMatcher(['/documents/:path*']);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
